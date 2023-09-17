27b5738efd702830fdf42912228556264e474fb4
Ho Hong Meng <melvinhohm@gmail.com>
2023-09-17 15:58:57 +0800

    updates on config and hohongmeng

--- app.py
+++ app.py
@@ -26,13 +26,11 @@ db_conn = connections.Connection(
 
 )
 output = {}
-table = 'employee'
-
+table = 'focsWebsite'
 
 @app.route('/')
 def index():
     return render_template('home.html', number=1)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
