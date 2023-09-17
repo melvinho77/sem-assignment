dbb51c5804f8ae9f9038c720b293d0f090fc39cf
Ho Hong Meng <melvinhohm@gmail.com>
2023-09-17 15:27:12 +0800

    Updates on config and home

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "focswebsite.cgtpcksgf7rv.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bacs3003"
-customdb = "focswebsite"
+customdb = "focsWebsite"
 custombucket = "hohongmeng-bucket"
 customregion = "us-east-1"
