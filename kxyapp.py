20b07bde1c4559b71903d4cdd277d236ab1fc5ca
kuxinyau <kuxinyau@gmail.com>
2023-09-17 14:31:24 +0800

    12

---
+++ kxyapp.py
@@ -0,0 +1,31 @@
+from flask import render_template, make_response
+from flask import redirect
+import mimetypes
+from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from botocore.exceptions import ClientError
+from pymysql import connections
+import boto3
+from config import *
+import datetime
+
+app = Flask(__name__)
+app.secret_key = 'sem'
+
+bucket = custombucket
+region = customregion
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+
+)
+output = {}
+table = 'focs'
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
