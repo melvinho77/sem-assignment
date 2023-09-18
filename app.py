fcdc741f79f112278a8688d8def49f72c6313399
yongsean <88657089+yongsean@users.noreply.github.com>
2023-09-18 19:57:45 +0800

    。。

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import render_template, make_response
+from flask import render_template, make_response, jsonify
 from flask import redirect
 import mimetypes
 from flask import Flask, render_template, request, redirect, url_for, session, send_file
 from botocore.exceptions import ClientError
 from pymysql import connections
 import boto3
+import socket
 from config import *
 import datetime
 import difflib
@@ -24,66 +25,111 @@ db_conn = connections.Connection(
     user=customuser,
     password=custompass,
     db=customdb
-
 )
 output = {}
 table = 'employee'
 
 @app.route('/home_page')
 def home_page():
-    # Add any logic here if needed
     return render_template('home.html')
 
 @app.route('/')
 def index():
-    return render_template('home.html', number=1)
-
-@app.route('/login_application')
-def login_application():
-    return render_template('studentLogin.html')
+    network_details = get_network_details()
+    return render_template('home.html', number=1, network_details=network_details)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    network_details = get_network_details()
+    return render_template('home.html', network_details=network_details)
 
+# req1
+@app.route('/relevantResult_display', methods=['POST'])
+def relevantResult_display():
+    result = request.form['result']
+    network_details = get_network_details()
+    return render_template(result, network_details=network_details)
+
+# req1
 @app.route('/homeSearchProgramme', methods=['POST'])
 def homeSearchProgramme():
     searchObj = request.form['textInput']
-    
+    network_details = get_network_details()
+
     select_sql = f"SELECT * FROM availableProgramme"
     cursor = db_conn.cursor()
     cursor.execute(select_sql)
     searchRange = cursor.fetchall()
 
-    url_set = {"programmes/Diploma in Computer Science.html"}
+    url_set = {
+        1: "programmes/Diploma in Computer Science.html",
+        2: "programmes/Diploma in Information Systems.html",
+        3: "programmes/Diploma in Information Technology.html",
+        4: "programmes/Diploma in Software Engineering.html",
+        5: "programmes/Management Mathematics with Computing.html",
+        6: "programmes/Interactive Software Technology.html",
+        7: "programmes/Data Science.html",
+        8: "programmes/Enterprise Information Systems.html",
+        9: "programmes/Information Security.html",
+        10: "programmes/Internet Technology.html",
+        11: "programmes/Software Systems Development.html",
+        12: "programmes/Software Engineering.html",
+        13: "programmes/Master of Computer Science.html",
+        14: "programmes/Master of Information Technology.html",
+        15: "programmes/Master of Science in Mathematical Sciences.html",
+        16: "programmes/Doctor of Philosophy Computer Science.html",
+        17: "programmes/Doctor of Philosophy Information Technology.html",
+        18: "programmes/Doctor of Philosophy Mathematical Sciences.html"
+    }
+
     similarity_scores = []
 
     # Iterate through the program names in 'searchRange'
     for program in searchRange:
-        print(program)
+        program_id = program[0]
         program_name = program[1]
         similarity = difflib.SequenceMatcher(None, searchObj, program_name).ratio()
 
         if similarity > 0.1:
-            similarity_scores.append((program_name, similarity))
+            similarity_scores.append((program_id, program_name, similarity))
 
     # Sort the results by similarity in descending order
-    similarity_scores.sort(key=lambda x: x[1], reverse=True)
+    sorted_similarity_scores = sorted(similarity_scores, key=lambda x: x[2], reverse=True)
 
     # Get the top 5 most relevant results
-    top_5_results = similarity_scores[:5]
+    top_5_results = sorted_similarity_scores[:5]
 
+    relevantResults = []
     if not top_5_results:
-        return("no relevant result")
+        return render_template('relevantProgrammeSearchResult.html', relevantResults = relevantResults, network_details=network_details)
     else:
+        for element in top_5_results:
+            count = 0
+            for url in url_set:
+                count += 1
+                if count == element[0]:
+                    relevantResults.append(url_set.get(count))
+        print(relevantResults)
+        return render_template('relevantProgrammeSearchResult.html', relevantResults = relevantResults, network_details=network_details)
 
+# N8 - Retrieve network details
+def get_network_details():
+    try:
+        # Get the host name of the local machine
+        host_name = socket.gethostname()
 
+        # Get both IPv4 and IPv6 addresses associated with the host
+        ipv4_address = socket.gethostbyname(host_name)
+        ipv6_address = socket.getaddrinfo(host_name, None, socket.AF_INET6)[0][4][0]
 
+        return {
+            'Host Name': host_name,
+            'IPv4 Address': ipv4_address,
+            'IPv6 Address': ipv6_address
+        }
+    except Exception as e:
+        return {'Error': str(e)}
 
-
-
-
-        return top_5_results
 
 
 
