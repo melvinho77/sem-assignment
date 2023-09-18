73268890fb13e8993fe03c4d1c1c2c4cd169c990
Kl0603 <lokekok47@gmail.com>
2023-09-19 00:54:40 +0800

    a

--- app.py
+++ app.py
@@ -142,7 +142,6 @@ def showAllProgramme():
     except Exception as e:
         return str(e)
     
-    return findCourseNoInclude(2)
     return render_template('compareProgramme.html', 
                            course_list=course_list,
                            course1=findCourse(1),
