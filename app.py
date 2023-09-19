7fcaaf7b67a370d661c598cbfbf5283b5bfd60b4
Kl0603 <lokekok47@gmail.com>
2023-09-19 18:09:06 +0800

    a

--- app.py
+++ app.py
@@ -335,9 +335,7 @@ def findSameCourse():
 
 def findNotExitsCourse(programmeId):
       #find all course
-    all_course = "SELECT DISTINCT courseTaken FROM programmeMainCourse "\
-                 "WHERE courseTaken NOT IN ( SELECT courseTaken FROM programmeMainCourse WHERE programmeId = 1)"\
-                 "ORDER BY courseTaken;"
+    all_course = "SELECT DISTINCT courseTaken FROM programmeMainCourse WHERE courseTaken NOT IN ( SELECT courseTaken FROM programmeMainCourse WHERE programmeId = %s) ORDER BY courseTaken;"
     cursor_Allcourse = db_conn.cursor()
     
     try:
